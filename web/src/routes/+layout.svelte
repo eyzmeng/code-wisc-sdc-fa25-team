45dbb39fe3f1d924f016fbf75b35b525da9a6bee
Ethan Meng <uwisc@endfind.me>
2026-02-10 09:54:54 -0600

    site shutdown notice
    before i shut this down though maybe give
    a little heads-up.
    
    also i want to make it so that <CR> works,
    even though this is the most horrendous
    non-semantic HTML i've ever created....
    (or maybe i could try making it semantic?
    i don't know, eh.)

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -14,9 +14,18 @@
 
 <hr class="my-2" />
 <footer class="my-2">
-  <p class="text-center text-sm italic text-gray-500 m-auto w-full">
+  <p class="text-center text-sm italic text-gray-500 px-6 m-auto w-full">
     This site is run by <a href="https://rapidcow.github.io/site-wisc-sdc-fa25-team/"
       >SDC Fall 2025 Team 17</a
     >! Read our source code <a href="https://github.com/SDC-Fall-2025/Team-17-Club-Radar">here</a>.
+  </p>
+  <p class="text-center"></p>
+  <p class="text-center text-gray-500 px-6 m-auto w-2xl">
+    (<span class="text-red-500">NOTE: This site
+      (clubradar.endfindme.com) will be <strong>closed</strong>.
+      Although it will exist only for as long as
+      I find it useful to reference it, previous
+      Vercel builds deployed from GitHub CI will
+      be removed as soon as I am able.</span>)
   </p>
 </footer>
